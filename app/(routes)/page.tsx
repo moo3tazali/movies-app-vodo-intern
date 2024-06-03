e4871bc98592e20ce031c49eef4e30117b423393
moo3tazali <moo3tazali@gmail.com>
2024-06-03 04:28:12 +0300

    move the change page component to the end

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -6,18 +6,18 @@ import { ChangePages } from '@/components/home/change-pages';
 // Home page component definition
 export default function Home() {
   return (
-    <main className='p-4 min-h-[calc(100vh-169px)] flex flex-col'>
+    <main className='p-4 min-h-[calc(100vh-169px)] flex flex-col space-y-3'>
       <Heading title='Movies List' description='Choose your best movie' />
       {/* Heading component with title and description props */}
 
       <Search />
       {/* Search component for searching movies */}
 
-      <ChangePages />
-      {/* ChangePages component for pagination controls */}
-
       <Movies />
       {/* Movies component to display the list of movies */}
+
+      <ChangePages />
+      {/* ChangePages component for pagination controls */}
     </main>
   );
 }
